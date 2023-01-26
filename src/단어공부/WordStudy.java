8341fad2fd513f940afde4ffd3ff45ef0f9c904f
kyjin115 <kyjin115@naver.com>
2023-01-26 15:06:49 +0900

    API

---
+++ src/단어공부/WordStudy.java
@@ -0,0 +1,17 @@
+package 단어공부;
+// 알파벳 대소문자로 이루어진 단어가 주어지면 가장 많이 등장한 알파벳을 찾는 문제
+// 입력은 대소문자를 입력하고, 결과는 대소문자 구분 하지 않음
+// 만약 가장 많이 등장하는 알파벳이 여러개인 경우는 ? 로 출력
+// 결과 예시 : AaAbcdd => A
+// 결과 예시 : ddcfgrG => ?
+
+import java.util.Scanner;
+
+public class WordStudy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("알파벳 입력 : ");
+
+
+    }
+}
---
+++ src/열거타입예제/EnumTypeEx2.java
@@ -0,0 +1,12 @@
+package 열거타입예제;
+
+public class EnumTypeEx2 {
+    public static void main(String[] args) {
+       Developer developer = new Developer( "권영진", DevType.EMBEDDED, Career.SENIOR,Gender.MALE);
+       developer.devInfo();
+        Developer developer1 = new Developer( "곰돌이", DevType.DBA, Career.JUNIOR,Gender.MALE);
+        developer.devInfo();
+
+
+    }
+}
